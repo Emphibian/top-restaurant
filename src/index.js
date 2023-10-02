175c7842dabd2b92e23ad4fe9cc9517d13f2035f
Emphibian <RyuuSleek@protonmail.ch>
2023-10-02 12:03:45 +0530

    Add js to dynamically create content

---
+++ src/index.js
@@ -0,0 +1,36 @@
+let contentDiv = document.querySelector('#content');
+
+let headerDiv = document.createElement('div');
+let headerList = document.createElement('ul');
+
+function createListElement(text) {
+  let listElement = document.createElement('li');
+  listElement.textContent = text;
+  return listElement;
+}
+
+headerList.appendChild(createListElement('Link 1'));
+headerList.appendChild(createListElement('Link 1'));
+headerList.appendChild(createListElement('Link 1'));
+headerDiv.appendChild(headerList);
+console.log(contentDiv);
+contentDiv.appendChild(headerDiv);
+
+let bodyDiv = document.createElement('div');
+
+function createBlockDiv(img, text) {
+  let blockDiv = document.createElement('div');
+  let heading = document.createElement('h2');
+  let image = new Image(300);
+
+  image.src = img;
+  heading.textContent = text;
+
+  blockDiv.appendChild(heading);
+  blockDiv.appendChild(image);
+  return blockDiv;
+}
+
+bodyDiv.appendChild(createBlockDiv("./anh-nguyen-kcA-c3f_3FE-unsplash.jpg", "Try out the new dish!"));
+bodyDiv.appendChild(createBlockDiv("./shawnanggg-nmpW_WwwVSc-unsplash.jpg", "Who are we?"));
+contentDiv.appendChild(bodyDiv);
