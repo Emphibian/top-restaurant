dd8fd328ad2446f1ec5ead7db6cc5ff72a0f5088
Emphibian <RyuuSleek@protonmail.ch>
2023-10-06 11:06:06 +0530

    Fix: forgot to add contactLayout file

---
+++ src/contactLayout.js
@@ -0,0 +1,29 @@
+export function contactLayout() {
+  let mainBody = document.querySelector("main");
+
+  let bodyDiv = document.createElement("div");
+  let addressDiv = document.createElement("div");
+  let phonesDiv = document.createElement("div");
+
+  let heading = document.createElement("h2");
+  heading.textContent = "Phone numbers";
+  let number1 = document.createElement("p");
+  let number2 = document.createElement("p");
+  number1.textContent = "123456789";
+  number2.textContent = "123456788";
+  phonesDiv.appendChild(heading);
+  phonesDiv.appendChild(number1);
+  phonesDiv.appendChild(number2);
+
+  let addressHeading = document.createElement("h2");
+  addressHeading.textContent = "Address";
+  let addressPara = document.createElement("p");
+  addressPara.textContent =
+    "We reside in an unknown valley, we ourselves don't know";
+  addressDiv.appendChild(addressHeading);
+  addressDiv.appendChild(addressPara);
+
+  bodyDiv.appendChild(phonesDiv);
+  bodyDiv.appendChild(addressDiv);
+  mainBody.appendChild(bodyDiv);
+}
