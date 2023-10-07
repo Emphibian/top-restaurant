52043d672c99ea856ec67c057fc1d611a7e3e5b0
Emphibian <RyuuSleek@protonmail.ch>
2023-10-07 12:06:55 +0530

    Add css to the contact tab

--- src/contactLayout.js
+++ src/contactLayout.js
@@ -2,20 +2,27 @@ export function contactLayout() {
   let mainBody = document.querySelector("main");
 
   let bodyDiv = document.createElement("div");
-  let addressDiv = document.createElement("div");
-  let phonesDiv = document.createElement("div");
-
+  bodyDiv.classList.add("contact");
   let heading = document.createElement("h2");
-  heading.textContent = "Phone numbers";
+  heading.textContent = "Contact Us Below";
+  mainBody.appendChild(heading);
+
+  let addressDiv = document.createElement("div");
+  addressDiv.classList.add("address");
+  let phonesDiv = document.createElement("div");
+  phonesDiv.classList.add("phone");
+
+  let phoneHeading = document.createElement("h3");
+  phoneHeading.textContent = "Phone numbers";
   let number1 = document.createElement("p");
   let number2 = document.createElement("p");
   number1.textContent = "123456789";
   number2.textContent = "123456788";
-  phonesDiv.appendChild(heading);
+  phonesDiv.appendChild(phoneHeading);
   phonesDiv.appendChild(number1);
   phonesDiv.appendChild(number2);
 
-  let addressHeading = document.createElement("h2");
+  let addressHeading = document.createElement("h3");
   addressHeading.textContent = "Address";
   let addressPara = document.createElement("p");
   addressPara.textContent =
